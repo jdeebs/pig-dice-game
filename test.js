16aa5acdbbb819ea7f608fe6984a65068566d34b
Jacob DiBiase <jakesback93@hotmail.com>
2025-05-14 18:22:37 -0700

    Initial commit

---
+++ test.js
@@ -0,0 +1,13 @@
+// Connecting DOM
+const rollBtn = document.getElementById("rollBtn");
+const holdBtn = document.getElementById("holdBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+// Declaring states for the game
+// Build Dice Roll
+
+
+// Player holds
+// Updating Display
+// Switch players
+
